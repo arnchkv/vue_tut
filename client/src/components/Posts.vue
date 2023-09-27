f794c41c2d834a0148334113ff728d6c8918a6ae
Arnav <arnchkv@gmail.com>
2023-09-27 13:11:31 +0530

    onMounted added, post listing not working

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -8,76 +8,92 @@
         <button v-if="isEditing" @click="updatePost">Update</button>
         <button v-if="isEditing" @click="cancelEdit">Cancel</button>
 
+        <!-- create posts -->
         <button v-else @click="createPost">Create</button>
+
+        <!-- list posts -->
+        <div v-for="post in posts" :key="post.id">
+            <h5>[{{ post.id }}] {{ post.title }}</h5>
+            <p>{{ post.body }}</p>
+
+            <button @click="editPost(post.id)">Edit</button>
+            <button @click="deletePost(post.id)">Delete</button>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+    import { ref, onMounted } from 'vue';
 
-const posts = ([])
+    const posts = ([])
+    const title = ref('')
+    const body = ref('')
+    const post_id = ref(0)
+    const isEditing = ref(false)
+    const API_URL = "http://localhost:3000/posts"
 
-const title = ref('')
-const body = ref('')
-const post_id = ref(0)
-const isEditing = ref(false)
-const API_URL = "http://localhost:3000/posts"
 
-const createPost = async () => {
-    // console.log(title.value)
-    // console.log(body.value)
-
-    const res = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: title.value,
-            body: body.value
-        })
+    onMounted(async () => {
+        const res = await fetch(API_URL)
+        posts.value = await res.json()
+        console.log({res, posts})
     })
 
-    const data = await res.json()
+    const createPost = async () => {
+        // console.log(title.value)
+        // console.log(body.value)
 
-    posts.push(data)
-    title.value = ""
-    body.value = ""
-    post_id.value = 0
-}
+        const res = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title.value,
+                body: body.value
+            })
+        })
 
-const updatePost = async () => {
-    return true;
-}
+        const data = await res.json()
 
-const cancelEdit = () => {
-    return true;
-}
+        posts.value.push(data)
+        title.value = ""
+        body.value = ""
+        post_id.value = 0
+    }
+
+    const updatePost = async () => {
+        return true;
+    }
+
+    const cancelEdit = () => {
+        return true;
+    }
 </script>
 
 
 <style scoped>
-.title-input {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    background-color: #f8f8f8;
-    color: #111;
-    border-radius: 4px;
-    resize: vertical;
-}
+    .title-input {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        background-color: #f8f8f8;
+        color: #111;
+        border-radius: 4px;
+        resize: vertical;
+    }
 
-.body-input {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    background-color: #f8f8f8;
-    color: #111;
-    border-radius: 4px;
-    resize: vertical;
-}
+    .body-input {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        background-color: #f8f8f8;
+        color: #111;
+        border-radius: 4px;
+        resize: vertical;
+    }
 </style>
