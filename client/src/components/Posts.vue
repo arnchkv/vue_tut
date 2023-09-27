a147d866075bd0a45070463abf83d6d5194a5ee6
Arnav <arnchkv@gmail.com>
2023-09-27 13:26:40 +0530

    post create error solved

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -5,7 +5,7 @@
         <input type="text" name="title" id="title" v-model="title" placeholder="Title" class="title-input">
         <input type="text" name="body" id="body" v-model="body" placeholder="Body" class="body-input">
 
-        <button v-if="isEditing" @click="updatePost">Update</button>
+                <button v-if="isEditing" @click="updatePost">Update</button>
         <button v-if="isEditing" @click="cancelEdit">Cancel</button>
 
         <!-- create posts -->
@@ -23,77 +23,77 @@
 </template>
 
 <script setup>
-    import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
-    const posts = ([])
-    const title = ref('')
-    const body = ref('')
-    const post_id = ref(0)
-    const isEditing = ref(false)
-    const API_URL = "http://localhost:3000/posts"
+const posts = ref([])
+const title = ref('')
+const body = ref('')
+const post_id = ref(0)
+const isEditing = ref(false)
+const API_URL = "http://localhost:3000/posts"
 
 
-    onMounted(async () => {
-        const res = await fetch(API_URL)
-        posts.value = await res.json()
-        console.log({res, posts})
-    })
+onMounted(async () => {
+    const res = await fetch(API_URL)
+    posts.value = await res.json()
+console.log({res, posts})
+})
 
-    const createPost = async () => {
-        // console.log(title.value)
+const createPost = async () => {
+// console.log(title.value)
         // console.log(body.value)
 
-        const res = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title: title.value,
-                body: body.value
-            })
+    const res = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title.value,
+            body: body.value
         })
+    })
 
-        const data = await res.json()
+    const data = await res.json()
 
-        posts.value.push(data)
-        title.value = ""
-        body.value = ""
-        post_id.value = 0
-    }
+    posts.value.push(data)
+    title.value = ""
+    body.value = ""
+    post_id.value = 0
+}
 
-    const updatePost = async () => {
-        return true;
-    }
+const updatePost = async () => {
+    return true;
+}
 
-    const cancelEdit = () => {
-        return true;
-    }
+const cancelEdit = () => {
+    return true;
+}
 </script>
 
 
 <style scoped>
-    .title-input {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        background-color: #f8f8f8;
-        color: #111;
-        border-radius: 4px;
-        resize: vertical;
-    }
+.title-input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    background-color: #f8f8f8;
+    color: #111;
+    border-radius: 4px;
+    resize: vertical;
+}
 
-    .body-input {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        background-color: #f8f8f8;
-        color: #111;
-        border-radius: 4px;
-        resize: vertical;
-    }
+.body-input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    background-color: #f8f8f8;
+    color: #111;
+    border-radius: 4px;
+    resize: vertical;
+}
 </style>
